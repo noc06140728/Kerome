5746ab620324f1571fd748ade442427bcb028e25
mnanba <mnanba@mnanba.rsol.local>
2011-04-06 12:25:13 +0900

    コミット

--- src/net/hardwave/kerome/Kerome.java
+++ src/net/hardwave/kerome/Kerome.java
@@ -2,7 +2,6 @@ package net.hardwave.kerome;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Formatter;
 import java.util.GregorianCalendar;
 
 import android.app.Activity;
@@ -10,12 +9,10 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
