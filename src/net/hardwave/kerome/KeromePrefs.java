aff157e3ee136503fcd4b17a3840103bbb5bd0a7
mnanba <mnanba@mnanba.rsol.local>
2011-04-08 18:36:26 +0900

    設定画面の宛先のサマリーに、設定しているメールアドレスを表示するように修正した。

--- src/net/hardwave/kerome/KeromePrefs.java
+++ src/net/hardwave/kerome/KeromePrefs.java
@@ -1,15 +1,33 @@
 package net.hardwave.kerome;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.preference.Preference;
+import android.preference.PreferenceManager;
+import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceActivity;
 
 public class KeromePrefs extends PreferenceActivity {
 
+	private EditTextPreference recipientEditText;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.prefs);
 		setResult(RESULT_OK, null);
+		
+		SharedPreferences myPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+		recipientEditText = (EditTextPreference) findPreference(getString(R.string.prefs_recipient));
+		recipientEditText.setSummary(myPrefs.getString(getString(R.string.prefs_recipient), getString(R.string.recipient_summary)));
+		
+		recipientEditText.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+			public boolean onPreferenceChange(Preference preference, Object newValue) {
+				((EditTextPreference)preference).setSummary((String)newValue.toString());
+				return true;
+			}
+		});
 	}
 
 }
